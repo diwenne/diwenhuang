79dc043a2956f01d33b3387d63028d2e217dbc03
diwenne <diwenh5@icloud.com>
2025-08-14 08:22:17 -0700

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Home from './pages/Home';
 import Work from './pages/Work';
 import Projects from './pages/Projects';
 import CcimCaseStudy from './pages/CcimCaseStudy';
-import SmashSpeedCaseStudy from './pages/SmashspeedCaseStudy'; // 1. Import the new component
+import SmashSpeedCaseStudy from './pages/SmashspeedCaseStudy';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/work" element={<Work />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/projects/ccim" element={<CcimCaseStudy />} />
-      {/* 2. Add the new route for the Smashspeed case study */}
-      <Route path="/projects/smashspeed-engine" element={<SmashSpeedCaseStudy />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/ccim" element={<CcimCaseStudy />} />
+        <Route path="/projects/smashspeed-engine" element={<SmashSpeedCaseStudy />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
